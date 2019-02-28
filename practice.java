1b176fd064dc8ccc517aa69c4ce393428a5de8e2
Bhuwan KC <alsatianbhuwan7@gmail.com>
2019-02-28 01:30:24 -0600

    Documented Maps

--- practice.java
+++ practice.java
@@ -1,12 +1,28 @@
 
-import java.util.Arrays;
+import java.util.*;
 
 public class practice
 {
 	public static void main(String[] args)
 	{
-		int[] numbers = {1,2,3,4,5};
+		ArrayList<Integer> prime = new ArrayList<>();
+		prime.add(2);
+		prime.add(3);
+		prime.add(5);
+		prime.add(7);
 		
-		System.out.printf("%s\n",numbers[0]);
+		ArrayList<Integer> num = new ArrayList<>(prime);
+		num.set(0,0);
+		
+		print(prime);
+		print(num);
+	}
+	
+	public static void print(ArrayList<Integer> al)
+	{
+		for(int i=0; i<al.size(); i++)
+		{
+			System.out.println(al.get(i));
+		}
 	}
 }
